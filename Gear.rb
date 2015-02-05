529e5b2fcf3291be4c97419f8d9e3a1a51ef1d92
Kevin Matthews <KevinNotMatt@gmail.com>
2015-02-04 20:31:19 -0800

    Comments

--- Gear.rb
+++ Gear.rb
@@ -12,6 +12,14 @@ puts ratio  #1.11
 =end
 
 class Gear
+  #Using accessor methods encapsulates data into behavior
+  #Your program can now reference behavior, which is defined in
+  #only one spot, the accessor method, e.g.
+  #def cog
+    #@cog
+  #end
+  #If the definition of a cog ever changes, you need only change
+  #this one definition rather than every reference to the variable.
   attr_reader :chainring, :cog, :rim, :tire
 
   def initialize(chainring, cog, rim, tire)
@@ -22,7 +30,10 @@ class Gear
   end
 
   def ratio
+    #Notice that this uses accessor methods
+    #instead of referring directly to instance variables
     chainring / cog.to_f
+    #Not @chainring / @cog.to_f
   end
 
   def gear_inches # wheel diameter * gear ratio
