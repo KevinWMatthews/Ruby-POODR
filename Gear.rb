f72f79c26fddfcd68f0129f59fd08d8e54e52906
Kevin Matthews <KevinNotMatt@gmail.com>
2015-02-04 20:02:45 -0800

    A simple Gear class and demo script

--- Gear.rb
+++ Gear.rb
@@ -1,9 +1,25 @@
+=begin
+#  An example of bicycle gearings:
 chainring = 52
 cog       = 11
-ratio     = chainring/cog.to_f
-puts ratio
+ratio     = chainring / cog.to_f
+puts ratio  #4.72
 
 chainring = 30
 cog       = 27
 ratio     = chainring / cog.to_f
-puts ratio
+puts ratio  #1.11
+=end
+
+class Gear
+  attr_reader :chainring, :cog
+
+  def initialize(chainring, cog)
+    @chainring = chainring
+    @cog = cog
+  end
+
+  def ratio
+    chainring / cog.to_f
+  end
+end
---
+++ GearDemo.rb
@@ -0,0 +1,8 @@
+=begin
+  Demonstrate various aspects and properties of the Gear class
+=end
+
+require_relative 'Gear'
+
+puts Gear.new(52, 11).ratio
+puts Gear.new(30, 27).ratio
