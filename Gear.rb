1283598cbbe1a3fe497ec101840490515757e6de
Kevin Matthews <KevinNotMatt@gmail.com>
2015-02-07 14:34:31 -0800

    Prep for next example

--- Gear.rb
+++ Gear.rb
@@ -12,16 +12,11 @@ puts ratio  #1.11
 =end
 
 class Gear
-  #Using accessor methods encapsulates data into behavior
-  #Your program can now reference behavior, which is defined in
-  #only one spot, the accessor method, e.g.
-  #def cog
-    #@cog
-  #end
-  #If the definition of a cog ever changes, you need only change
-  #this one definition rather than every reference to the variable.
   attr_reader :chainring, :cog, :wheel
 
+  #Notice that all users of Gear depend on the order of
+  #the initializer arguments.
+  #We can break this dependency using a hash and symbols.
   def initialize(chainring, cog, wheel=nil)
     @chainring = chainring
     @cog = cog
@@ -29,30 +24,18 @@ class Gear
   end
 
   def ratio
-    #Notice that this uses accessor methods
-    #instead of referring directly to instance variables
     chainring / cog.to_f
-    #Not @chainring / @cog.to_f
   end
 
   def gear_inches
-    #Notice that this still refers to wheel.
-    #This dependency could spread or be hard to find
-    # wheel.diameter * ratio
-
-    #gear_inches now sends messages only to self
     diameter * ratio
   end
 
-  #Isolate this message dependency
   def diameter
     wheel.diameter
   end
 end
 
-#Refactoring to include a circumference method
-#There is now a thoroughly convincing reasont to have Wheel
-#independent of Gear.
 class Wheel
   attr_reader :rim, :tire
   def initialize(rim, tire)
@@ -66,35 +49,5 @@ class Wheel
 
   def circumference
     diameter * Math::PI
-  end
-end
-
-#The gear class isn't using WheelData; this was a separate example.
-class WheelData
-  attr_reader :wheels
-  def initialize(data)
-    @wheels = wheelify(data)
-  end
-
-  def diameters
-    #Split up the responsibility of iterating and calculating
-    wheels.collect { |wheel| diameter(wheel) }
-  end
-
-  def diameter(wheel)
-    #We now have meaningful, abstract references to data
-    #using accessor methods. diameters doesn't know anything
-    #about how wheel data is stored
-    wheel.rim + (wheel.tire * 2)
-  end
-
-  #A Struct is used because the attributes are fixed.
-  #rim and tire are accessor methods
-  Wheel = Struct.new(:rim, :tire)
-  def wheelify(data)
-    #The structure of the array is now buried here.
-    #It is hidden using the wheelify method.
-    data.collect { |cell|
-      Wheel.new(cell[0], cell[1]) }
   end
 end
--- GearDemo.rb
+++ GearDemo.rb
@@ -3,26 +3,3 @@
 =end
 
 require_relative 'Gear'
-
-# puts Gear.new(52, 11).ratio #This gets broken!
-# puts Gear.new(30, 27).ratio
-
-# puts Gear.new(52, 11, 26, 1.5).gear_inches
-# puts Gear.new(52, 11, 24, 1.25).gear_inches
-
-puts "Wheel diameters:"
-puts WheelData.new([[622, 20], [622, 23], [559, 30], [559, 40]]).diameters
-puts
-
-#why the @ ?
-@wheel = Wheel.new(26, 1.5)
-puts "Wheel circumference:"
-puts @wheel.circumference
-puts "Wheel diameter:"
-puts @wheel.diameter
-
-puts "Gear inches:"
-puts Gear.new(52, 11, @wheel).gear_inches
-
-puts "Ratio (which doesn't need a wheel):"
-puts Gear.new(52, 11).ratio
