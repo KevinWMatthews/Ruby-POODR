e7d0c98ba44966225946f213ca0d32c7a0e6bb6b
Kevin Matthews <KevinNotMatt@gmail.com>
2015-02-11 19:40:51 -0800

    Refactored example gets worse: more dependencies

--- DuckTyping.rb
+++ DuckTyping.rb
@@ -1,14 +1,26 @@
 # A demonstration of duck typing, starting with a bad example
+#
+#Things get more complicated: add a TripCoordinator and Driver class
 
 class Trip
   attr_reader :bicycles, :customers, :vehicle
 
-  # this 'mechanic' argument could be of any class
+  # The preparer argument could be of any class
   # If you happen to pass an instance of Mechanic (see below), it works.
-  def prepare(mechanic)
-    # prepare() has no explicit dependency on Mechanics but its argument must
-    # respond to prepare_bicycles
-    mechanic.prepare_bycicles(bicycles)
+  def prepare(preparers)
+    #The argument was set up to handle a Mechanic
+    #Now it must be expanded to include every kind of preparer.
+    preparers.each {|preparer|
+      case preparer
+      when Mechanic
+        preparer.prepare_bicycles(bicycles)
+      when TripCoordinator
+        preparer.buy_food(customers)
+      when Driver
+        preparer.gas_up(vehicle)
+        preparer.fill_water_tank(vehicle)
+      end
+    }
   end
 
   #...
@@ -24,4 +36,18 @@ class Mechanic
   end
 end
 
-#Draw a sequence diagram to see what's wrong or see figure 5.1
+class TripCoordinator
+  def buy_food(customers)
+    #...
+  end
+end
+
+class Driver
+  def gas_up(vehicle)
+    #...
+  end
+
+  def fill_water_tank(vehicle)
+    #...
+  end
+end
